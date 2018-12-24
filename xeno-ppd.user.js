e551e92361516fd537b929959ad0fe64ddeab67f
radio <radio.github@gmail.com>
2018-12-23 19:50:36 -0800

    fixes compatibility issues with xeno v2

--- xeno-ppd.user.js
+++ xeno-ppd.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
-// @name        ResetEra: Display Posts Per Day
+// @name     ResetEra: Display Posts Per Day
 // @description Displays posts per day average
-// @version     2.1
-// @match       *://*.resetera.com/members/*
-// @match       *://*.resetera.com/threads/*
-// @match       *://*.neogaf.com/members/*
-// @match       *://*.neogaf.com/threads/*
-// @grant       GM.log
-// @namespace   hateradio)))
+// @version  3
+// @grant    none
+// @match    *://*.resetera.com/members/*
+// @match    *://*.resetera.com/threads/*
+// @match    *://*.neogaf.com/members/*
+// @match    *://*.neogaf.com/threads/*
+// @grant    GM.log
+// @namespace hateradio)))
 // ==/UserScript==
 
 (() => {
@@ -17,11 +18,12 @@
 
         constructor(selector) {
             this.selector = selector
+            console.log(this.selector)
             let { joined, messages } = this.rawData
             console.log('Found date/messages:', joined, messages)
 
             this.messages = Posts.toInt(messages)
-            this.joined = Posts.parseDate(joined)
+            this.joined = new Date(joined)
 
             console.log('Detected date:', this.joined.toString())
         }
@@ -31,7 +33,7 @@
         }
 
         template(title, body) {
-            return `<dl><dt>${title}:</dt><dd>${body}</dd></dl>`
+            return `<dl class="pairs pairs--rows pairs--rows--centered re_headerStats--joined"><dt>${title}:</dt><dd>${body}</dd></dl>`
         }
 
         append() {
@@ -47,8 +49,9 @@
             console.log(this.selector, this.parent)
             return Array.from(this.parent.querySelectorAll('dl'))
                 .reduce((o, d) => {
-                    const txt = d.textContent.split(':')
-                    o[txt[0].trim().toLowerCase().replace(':', '')] = txt[1].trim()
+                    const prop = d.querySelector('dt').textContent.trim().toLowerCase()
+                    const content = d.querySelector('time') ? d.querySelector('time').dateTime : d.querySelector('dd').textContent.trim()
+                    o[prop] = content
                     return o
                 }, {})
         }
@@ -56,15 +59,6 @@
         get average() {
             const days = this.diff(this.joined, new Date())
             return this.messages / days
-        }
-
-        static parseDate(date) {
-            date = date.split(' ')
-            const month = Posts.MONTHS[date[0]]
-            const day = Posts.toInt(date[1])
-            const year = Posts.toInt(date[2])
-
-            return new Date(year, month, day)
         }
 
         static toInt(str) {
@@ -77,7 +71,7 @@
             if (path === 'threads') {
                 ModalPosts.listener()
             } else {
-                const p = new Posts('.secondaryContent.pairsJustified')
+                const p = new Posts('div.re_headerStats')
                 p.append()
             }
         }
@@ -87,39 +81,31 @@
     class ModalPosts extends Posts {
 
         constructor(id) {
-            super(`#memberCard${id}`);
+            super(`.memberTooltip-stats dd a[href*="user_id=${id}"]`);
         }
 
         template(title, body) {
-            return `<dt>${title}:</dt><dd>${body}</dd>`
+            return `<dl class="pairs pairs--rows pairs--rows--centered re_mainStat"><dt>${title}</dt><dd>${body}</dd></dl>`
         }
 
-        append() {
-            const temp = this.template('Posts Per Day', this.average.toFixed(2))
-            this.parent.querySelector('.userStats.pairsInline').insertAdjacentHTML('beforeend', temp)
-        }
-
-        get rawData() {
-            const data = Array.from(this.parent.querySelectorAll('dd')).map(e => e.textContent)
-            return {
-                joined: data[0],
-                messages: data[1]
-            }
+        get parent() {
+            return document.querySelector(this.selector).closest('div')
         }
 
         static listener() {
-            document.body.addEventListener('click', (e) => {
-                let { target } = e
+            document.body.addEventListener('click', e => {
+                let { target = null } = e
                 if (e && target && target.tagName === 'IMG') {
                     target = target.parentElement
                 }
-                console.log('target', target)
+                //console.log('target', target)
                 const id = e && target && target.href && parseInt(target.href.split('.').pop(), 10)
+                console.log('id', id)
                 if (id && !ModalPosts.ID[id]) {
                     window.setTimeout(() => {
                         const p = new ModalPosts(id);
                         p.append()
-                    }, 1000)
+                    }, 2000)
                     ModalPosts.ID[id] = true
                 }
             }, false)
@@ -130,25 +116,10 @@
 
     Posts.MILLS_PER_DAY = 24 * 60 * 60 * 1000
 
-    Posts.MONTHS = {
-        Jan: 0,
-        Feb: 1,
-        Mar: 2,
-        Apr: 3,
-        May: 4,
-        Jun: 5,
-        Jul: 6,
-        Aug: 7,
-        Sep: 8,
-        Sept: 8,
-        Oct: 9,
-        Nov: 10,
-        Dec: 11
-    }
-
     ModalPosts.ID = {}
 
     // window.setInterval(Posts.main, 5000)
     Posts.main()
+
 
 })()
